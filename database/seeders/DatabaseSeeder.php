b88cd0c53b5e2a86dea89cf3bc268b08b36af665
alfaiza <izzaalfaiza@yahoo.co.id>
2023-07-30 00:04:45 +0800

    tambah seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,6 +15,42 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        DB::table('users')->insert([
+            ['name' => 'Admin',
+            'email' => 'superadmin@gmail.com',
+            'password' => Hash::make('111'),
+            'nip' => '111',
+            'unit' => 'Perwakilan BPKP Provinsi NTB',
+            'level' => '1',
+            'bidang' => 'Umum'
+            ],
+            [
+            'name' => 'Komang',
+            'email' => 'komanga@gmail.com',
+            'password' => Hash::make('222'),
+            'nip' => '222',
+            'unit' => 'Perwakilan BPKP Provinsi NTB',
+            'level' => '2',
+            'bidang' => 'Umum'
+            ],
+            [
+            'name' => 'Yusuf',
+            'email' => 'yusuf@gmail.com',
+            'password' => Hash::make('333'),
+            'nip' => '333',
+            'unit' => 'Perwakilan BPKP Provinsi NTB',
+            'level' => '3',
+            'bidang' => 'APD'
+            ],
+            [
+            'name' => 'Tia',
+            'email' => 'yusuf@gmail.com',
+            'password' => Hash::make('444'),
+            'nip' => '444',
+            'unit' => 'Perwakilan BPKP Provinsi NTB',
+            'level' => '4',
+            'bidang' => 'APD'
+            ]
+        ]);
     }
 }
