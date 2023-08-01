4bc74869cdd464944f3fe3330aefbeb5b030bccd
alfaiza <izzaalfaiza@yahoo.co.id>
2023-08-01 10:32:16 +0800

    tambah seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -21,35 +21,8 @@ class DatabaseSeeder extends Seeder
             'password' => Hash::make('111'),
             'nip' => '111',
             'unit' => 'Perwakilan BPKP Provinsi NTB',
-            'level' => '1',
-            'bidang' => 'Umum'
-            ],
-            [
-            'name' => 'Komang',
-            'email' => 'komanga@gmail.com',
-            'password' => Hash::make('222'),
-            'nip' => '222',
-            'unit' => 'Perwakilan BPKP Provinsi NTB',
-            'level' => '2',
-            'bidang' => 'Umum'
-            ],
-            [
-            'name' => 'Yusuf',
-            'email' => 'yusuf@gmail.com',
-            'password' => Hash::make('333'),
-            'nip' => '333',
-            'unit' => 'Perwakilan BPKP Provinsi NTB',
-            'level' => '3',
-            'bidang' => 'APD'
-            ],
-            [
-            'name' => 'Tia',
-            'email' => 'yusuf@gmail.com',
-            'password' => Hash::make('444'),
-            'nip' => '444',
-            'unit' => 'Perwakilan BPKP Provinsi NTB',
             'level' => '4',
-            'bidang' => 'APD'
+            'bidang' => 'Umum'
             ]
         ]);
     }
